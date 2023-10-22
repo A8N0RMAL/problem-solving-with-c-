682be4f8ee79b2559f9d593fc1d7e11c781145a3
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2023-10-23 02:40:52 +0300

    Update main.cpp

--- Contest#1/G. Katryoshka/main.cpp
+++ Contest#1/G. Katryoshka/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define c(x)            int x; cin>>x;
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
 using namespace std;
-int main() {
-	long long e, m, b;
-	cin >> e >> m >> b;
-	long long c = 0;
-	if (e == 0)
-		cout << 0;
-	else
-	{
-		if ((m >= e && m >= b) || (m >= e && m < b) || (m < e && m >= b))
-		{
-			c = min(e, b);
-			cout << c << endl;
-		}
-		else if ((m<e&&m<b))
-		{
-			c = m;
-			e -= m;
-			b -= m;
-			c += min(e / 2, b);
-			cout << c;
-		}
-	}
-	return 0;
+void test()
+{
+    ll e,m,b,k=0;
+    cin>>e>>m>>b;
+    ll mn=min(min(e,m),b);
+    k+=mn,e-=mn,m-=mn,b-=mn;    // cond. 3
+    mn=min(e/2,b);  // cond. 1
+    k+=mn;
+    cout<<k<<"\n";
+}
+int main()
+{
+    fast;
+    int t = 1; //cin>>t;
+    while(t--)
+    {
+        test();
+    }
+    return 0;
 }
