5d98bab43ba54e7986159d42ee782df56349c366
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2023-10-23 03:12:53 +0300

    Update main.cpp

--- Contest#1/H. Data Type Guessing/main.cpp
+++ Contest#1/H. Data Type Guessing/main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+/*#define ll long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define c(x)            ll x; cin>>x;
+*/
 using namespace std;
-int main() {
-	double n, k, a;
-	cin >> n >> k >> a;
-	double x = n * k / a;
-	long long y = x;
-	double z = x - y;
-	if (z>0 )
-		cout << "double\n";
-	else if (y<= 2147483647)
-		cout << "int\n";
-	else
-		cout << "long long\n";
-	return 0;
+
+void test()
+{
+    double n,k,a;
+    cin>>n>>k>>a;
+    double r=n*k/a;
+    long long x=r;
+    double z=r-x;
+    if(z>0)
+        cout<<"double\n";
+    else if(x<=2147483647)
+        cout<<"int\n";
+    else
+        cout<<"long long\n";
+}
+int main()
+{
+    fast;
+    //cin>>test;
+    test();
+    return 0;
 }
