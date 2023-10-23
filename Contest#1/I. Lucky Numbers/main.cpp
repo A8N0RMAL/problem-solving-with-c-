06cd7d2d959f36b61bcd4f29ffe1869d48ec916d
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2023-10-23 03:22:20 +0300

    Update main.cpp

--- Contest#1/I. Lucky Numbers/main.cpp
+++ Contest#1/I. Lucky Numbers/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define c(x)            ll x; cin>>x;
 using namespace std;
-int main() {
-	int x;
-	cin >> x;
-	if (x == 10||x==20 || x == 30 || x == 40 || x == 50 || x == 60 || x == 70 || x == 80 || x == 90) {
-		cout << "YES\n";
-		return 0;
-	}
-	int xa = x % 10;
-	x /= 10;
-	if (x % xa == 0 || xa % x == 0)
-		cout << "YES\n";
-	else
-		cout << "NO\n";
-	return 0;
+
+void test()
+{
+    c(x);   // 39
+    ll y=x%10;  // 39%10=9
+    ll z=x-y;    // 39-9=30
+    z/=10;
+    if(y%z==0 || z%y==0)
+        cout<<ye;
+    else
+        cout<<no;
+}
+int main()
+{
+    fast;
+    //cin>>test;
+    test();
+    return 0;
 }
