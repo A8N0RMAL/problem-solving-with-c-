db0b83fdf46c41ceb2a441360052680e637c01db
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2023-10-24 03:11:12 +0300

    Update main.cpp

--- Sheet#2/A. 1 to N/main.cpp
+++ Sheet#2/A. 1 to N/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define c(x)            ll x; cin>>x;
 using namespace std;
-int main() {
-	long long n;
-	cin >> n;
-	for (size_t i = 1; i <= n; i++)cout << i << "\n";
-	return 0;
+
+void test()
+{
+    c(n);
+    for(int i=1;i<=n;++i)
+        cout<<i<<"\n";
+}
+int main()
+{
+    fast;
+    //cin>>test;
+    test();
+    return 0;
 }
