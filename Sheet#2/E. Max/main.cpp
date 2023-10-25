0f3262b0c7cdbdefb5e207b48223c4e6d004ffad
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2023-10-25 04:06:07 +0300

    Update main.cpp

--- Sheet#2/E. Max/main.cpp
+++ Sheet#2/E. Max/main.cpp
@@ -1,16 +1,56 @@
+/******************************
+ *                            *
+ *    Author :  Mahmoud Saleh *
+ *    Created:  __.__.2023    *
+ *                            *
+ ******************************
+ *******************************************************
+ *                                                     *
+ *  ***-->           RE STUDENT             <--***     *
+ *  ***--> 	      MALWARE ANALYST           <--***     *
+ *  ***--> COMPUTER SCIENCE AND ENGINEERING <--***     *
+ *                                                     *
+ *******************************************************/
 #include <iostream>
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define int long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define c(x)            ll x; cin>>x;
+#define fl(i,n) for(int i=0;i<n;++i)
 using namespace std;
-int main() {
-	int n;
-	cin >> n;
-	int x = 0;
-	while (n--)
-	{
-		int c;
-		cin >> c;
-		if (c > x)
-			x = c;
-	}
-	cout << x << endl;
-	return 0;
+
+void test()
+{
+    c(n);
+    vi v(n);
+    fl(i,n)cin>>v[i];
+    sort(all(v));
+    cout<<v[n-1]<<"\n";
+}
+int32_t main()
+{
+    fast;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t=1;
+    while(t--)
+    {
+        test();
+    }
+    //w(t){test();}
+    return 0;
 }
