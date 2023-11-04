989d64c4c167509179eb057c3635e09d59741136
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2023-11-04 02:47:22 +0200

    Update main.cpp

--- Sheet#2/K. Divisors/main.cpp
+++ Sheet#2/K. Divisors/main.cpp
@@ -1,15 +1,58 @@
+/******************************
+ *                            *
+ *    Author :  Mahmoud Saleh *
+ *    Created:  __.__.2023    *
+ *                            *
+ ******************************
+ *******************************************************
+ *                                                     *
+ *  ***-->           RE STUDENT             <--***     *
+ *  ***--> 	      MALWARE ANALYST           <--***     *
+ *  ***--> COMPUTER SCIENCE AND ENGINEERING <--***     *
+ *                                                     *
+ *******************************************************/
 #include <iostream>
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define int long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define c(x)            ll x; cin>>x;
+#define fl(i,n) for(int i=0;i<n;++i)
 using namespace std;
-int main() {
-	int n;
-	cin >> n;
-	int x = n / 2;
-	cout << 1 << "\n";
-	for (size_t i = 2; i <= x; i++)
-	{
-		if (n % i == 0)
-			cout << i << "\n";
-	}
-	cout << n;
-	return 0;
+void printDivisors(int n)
+{
+    for (int i=1;i<=n;i++)
+        if (n%i==0)
+            cout<<i<<"\n";
+}
+void test()
+{
+    c(n);
+    printDivisors(n);
+}
+int32_t main()
+{
+    fast;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t=1;
+    while(t--)
+    {
+        test();
+    }
+    //w(t){test();}
+    return 0;
 }
