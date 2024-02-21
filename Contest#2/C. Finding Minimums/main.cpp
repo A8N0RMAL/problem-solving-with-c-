49f3f805033ea57f6cb348cbcbdaba4671863077
Mahmoud Saleh <119806250+A8N0RMAL@users.noreply.github.com>
2024-02-21 15:46:23 +0200

    Update main.cpp

--- Contest#2/C. Finding Minimums/main.cpp
+++ Contest#2/C. Finding Minimums/main.cpp
@@ -1,60 +1,182 @@
+/******************************
+ *                            *
+ *    Author :  Mahmoud Saleh *
+ *    Created:  __.__.2023    *
+ *                            *
+ ******************************
+ *******************************************************
+ *                                                     *
+ *  ***-->           RE STUDENT             <--***     *
+ *  ***--> 	      MALWARE ANALYST           <--***     *
+ *  ***--> COMPUTER SCIENCE AND ENGINEERING <--***     *
+ *                                                     *
+ *******************************************************/
 #include <iostream>
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define int long long
+#define ye  "YES\n"
+#define no  "NO\n"
+#define all(v) v.begin(), v.end()
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define c(x)            ll x; cin>>x;
+#define fl(i,n) for(int i=0;i<n;++i)
 using namespace std;
-int main() {
-	/*int n, k;
-	cin >> n >> k;
-	int mn;
-	int flag = 1; int c = 0;
-	for (size_t i = 1; i <= n; i++)
-	{
-		int val;
-		cin >> val;
-		if (flag)
-		{
-			mn = val;
-			flag = 0;
-		}
-		else
-		{
-			if (val<mn)
-				mn = val;
-		}
-		c++;
-		if (c==k||i==n)
-		{
-			cout << mn << " "; flag = 1;
-			c = 0;
-		}
-	}*/
+ll gcd(ll x, ll y)
+{
+    return (!y) ? x : gcd(y, x % y);
+}
+ll lcm(ll x, ll y)
+{
+    return ((x / gcd(x, y)) * y);
+}
+ll add(ll a, ll b, ll m)
+{
+    return ((a % m) + (b % m)) % m;
+}
+ll mul(ll a, ll b, ll m)
+{
+    return ((a % m) * (b % m)) % m;
+}
+ll sub(ll a, ll b, ll m)
+{
+    return ((a % m) - (b % m) + m) % m;
+}
+/*
+bool isPrime(ll n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-	int n;
-	cin >> n;
-	int k;
-	cin >> k;
-	int mn;
-	int f = 1;
-	int c = 0;
-	for (size_t i = 1; i <= n; i++)
-	{
-		int val;
-		cin >> val;
-		if (f)
-		{
-			mn = val;
-			f = 0;
-		}
-		else
-		{
-			if (val < mn)
-				mn = val;
-		}
-		c++;
-		if (c==k||i==n)
-		{
-			cout << mn << " ";
-			c = 0;
-			f = 1;
-		}
-	}
-	return 0;
+long long nCr(int n, int r) {
+    if (r > n)
+        return 0;
+    r = max(r, n - r);
+    long long ans = 1, div = 1, i = r + 1;
+    while (i <= n) {
+        ans *= i;
+        i++;
+        ans /= div;
+        div++;
+    }
+    return ans;
+}
+
+long long nPr(int n, int r) {
+    if (r > n)
+        return 0;
+    long long p = 1, i = n - r + 1;
+    while (i <= n) p *= i++;
+    return p;
+}
+
+ll maxSubstring(string s) {
+    ll n = s.length();
+    ll i = 0, j = 0;
+    ll maxLen = 0;
+    unordered_set<char> charSet;
+    while (j < n) {
+        if (charSet.find(s[j]) == charSet.end()) {
+            charSet.insert(s[j]);
+            maxLen = max(maxLen, j - i + 1);
+            j++;
+        } else {
+            charSet.erase(s[i]);
+            i++;
+        }
+    }
+    return maxLen;
+}
+*/
+/*
+All chars appear just once
+void setchars()
+{
+    ll f=1;
+    for(char c='a';c<='z';++c)
+    {
+        if(sett.find(c)==sett.end() && sett.find(c-'a'+'A')==sett.end())
+        {
+            f=0;
+            break;
+        }
+    }
+    if(f)
+        cout<<ye;
+    else
+        cout<<no;
+}
+*/
+/*
+void SumRange()
+{
+    c(l);c(r);
+    // Ensure that start is not greater than end
+    if(l > r)
+    {
+        ll tmp = l;
+        l=r;
+        r=tmp;
+    }
+    // u can use swap(l,r) <3
+    ll n = r - l + 1;
+    ll sum = (n * (l+r)) / 2;
+    cout<<sum<<endl;
+}
+*/
+
+void test()
+{
+    c(n);c(k); // 8 3
+    ll mn=INT_MAX, f=1, c=0;
+    fl(i, n)
+    {
+        // 4 -1 2 3 5 0 2 7
+        c(x);
+        if(f) // only z first time for each arr
+            mn=x, f=0; // mn=4  mn=-1
+        else
+        {
+            if(mn > x)
+                mn = x;
+            // mn=4 > x=-1 ? -> mn=-1
+            // mn=-1 > x=2 ? -> mn=-1
+        }
+        ++c; // 1 2 3
+        if(c==k || i==n-1) // c=k=3 then output mn and loop again
+        {
+            cout<<mn<<" ";
+            f=1,c=0;
+        }
+    }
+}
+int32_t main()
+{
+    fast;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t=1;
+    while(t--)
+    {
+        test();
+    }
+    // w(t){test();}
+    return 0;
 }
